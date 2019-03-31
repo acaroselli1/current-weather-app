3fc2ed6ce955b4694159d31ea04f350ebac958c5
Alexander Caroselli <alexandersylvia9@msn.com>
2019-03-30 18:46:27 -0600

    Now using https

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
     let inputValue = $("input").val();
     $.getJSON(
-      `http://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=imperial&APPID=8ddc6938c87f46e8454e0bbc7eef35b0`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=imperial&APPID=8ddc6938c87f46e8454e0bbc7eef35b0`,
       data => {
         console.log(data);
 
